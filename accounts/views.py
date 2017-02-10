39173163f0f61780dc934d2f58f219ac113755c3
a5810130 <a5810130@gmail.com>
2017-02-11 01:00:14 +0700

    index page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Account, List
 
 def index(request):
-    return HttpResponse("COMING SOON")
+    account_list = Account.objects.order_by('-create')
+    context = {'account_list':account_list}
+    return render(request, 'accounts/index.html', context)
