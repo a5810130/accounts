eaf72c26005b2fb2aa8541eb4638209d63046bef
a5810130 <a5810130@gmail.com>
2017-02-15 22:53:29 +0700

    -Add account

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^add_account/$', views.add_account, name='add_account'),
     url(r'^(?P<account_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,22 @@ def index(request):
 def detail(request, account_id):
     account = get_object_or_404(Account, pk=account_id)
     return render(request, 'accounts/detail.html', {'account':account, })
+
+def add_account(request):
+    try:
+        account_name = request.POST['account_name']
+        if not account_name.strip():
+            raise 
+    except :
+        account_list = Account.objects.order_by('-create')
+        context = {'account_list':account_list, 
+                   'error_message': "Account name is empty"}
+        return render(request, 'accounts/index.html', context)
+    else:
+        a = Account(name=account_name, create=timezone.now())
+        a.save()
+        return HttpResponseRedirect(reverse('accounts:index',))
+    
         
     
     
