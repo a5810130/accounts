155e8005c75a21cc5f4427e3fc9ded9d2928b81e
a5810130 <a5810130@gmail.com>
2017-02-16 00:02:38 +0700

    add list

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add_account/$', views.add_account, name='add_account'),
     url(r'^(?P<account_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<account_id>[0-9]+)/add_list/$', views.add_list, name='add_list'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
+from django.utils import timezone 
+from datetime import datetime
 
 from .models import Account, List
+
 
 def index(request):
     account_list = Account.objects.order_by('-create')
@@ -29,6 +31,30 @@ def add_account(request):
         a = Account(name=account_name, create=timezone.now())
         a.save()
         return HttpResponseRedirect(reverse('accounts:index',))
+    
+def add_list(request, account_id):
+    account = get_object_or_404(Account, pk=account_id)
+    try:
+        date_str = request.POST['date']
+        description = request.POST['description']
+        listType = request.POST['type']
+        value = request.POST['value']
+        if (not date_str.strip() or not description.strip()
+        or not value.strip()):
+            raise 
+    except :
+        return render(request, 'accounts/detail.html', 
+                {'account': account, 'message': "Incomplete information.",})
+    date = datetime.strptime(date_str,"%m/%d/%Y")
+    account.list_set.create(date=date, listType=listType,
+                            description=description,
+                            value=value)
+    account.save()
+    return render(request, 'accounts/detail.html', 
+                {'account': account,})
+        
+    
+    
     
         
     
